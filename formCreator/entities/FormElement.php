103dee1f3e39546fc92d752a8d2267c07408a755
dima <d.doroshchenko@gmail.com>
2016-10-22 14:08:42 +0300

    in progress

--- formCreator/entities/FormElement.php
+++ formCreator/entities/FormElement.php
@@ -8,13 +8,27 @@ class FormElement extends Entity
 {
     protected $name;
     protected $type;
-    protected $value;
+    protected $template;
 
+    public static $definition = array(
+        'text' => array('name'),
+        'textarea' => array('name'),
+        'dropdown' => array('name', 'values'),
+        'multiselect' => array('name', 'values'),
+        'radio' => array('name' => 'values'),
+        'switch' => array('name', 'values'),
+        'file' => array('name')
+    );
 
     public function setStorage($storage)
     {
         $this->storage = $storage;
         return $this;
+    }
+
+    public function getTemplate($type)
+    {
+        return 4;
     }
 }
 
--- formCreator/views/forms.php
+++ formCreator/views/forms.php
@@ -4,15 +4,22 @@
     </div>
     <div class="forms-block">
         <?php foreach ($forms as $form) { ?>
-            <div id="form_<?php echo $form->getID()?>">
-                <h4><?php echo $form->getName(); ?></h4>
-                <!--<form  name="<?/* echo $form['name']*/?>" action="<?/* echo $form['action']*/?>" type="<?/* echo $form['type']*/?>" method="<?/* echo $form['method']*/?>">
-                    <?php /*foreach ($form['form_elements'] as $element) {
-
-                    }
-                    */?>
-                </form>-->
-
+            <div id="form_<? echo $form->getID()?>">
+                <h2><? echo $form->getName(); ?></h2>
+                    <p><label> form name:</label><input type="text"  name="form[][name]" value ='<? echo $form->getName(); ?>'/></p>
+                    <p><label> form action:</label><input type="text"  name='form[][action]' value ='<? echo $form->getAction(); ?>'/></p>
+                    <p><label> form type:</label><input type="text"  name='form[][action]' value ='<? echo $form->getType(); ?>'/></p>
+                    <p><label> form method:</label><input type="text"  name='form[][action]' value ='<? echo $form->getmethod(); ?>'/></p>
+                    <div class="form_elements">
+                        <h3> Elements </h3>
+                        <? $elements = ($form->getElements()) ? $form->getElements() : array(); ?>
+                        <? foreach ($elements as $element) {?>
+                            <p>     <? var_dump(\formCreator\entities\FormElement::$definition);?>
+                                <label> element name:</label><input type="text"  name="form[][element][name]" value ='<? echo $element->getName(); ?>'/>
+                                <label> element type:</label><input type="<?echo $element->getType();?>"  name="form[][element][name]" value ='<? echo $element->getName(); ?>'/>
+                            </p>
+                        <?}?>
+                    </div>
             </div>
         <?php } ?>
     </div>
